92da2e791fd04f14e0a3752834b7f8d11bb24643
Florian <sicilia.florian02@gmail.com>
2023-08-09 11:01:07 +0200

    mock

--- client/src/pages/Home/components/UserCard/UserCard.tsx
+++ client/src/pages/Home/components/UserCard/UserCard.tsx
@@ -1,26 +1,35 @@
-import { useOutletContext, useNavigate } from 'react-router-dom';
-import { User, UserContext } from '@/interfaces';
+import { useOutletContext, useNavigate } from "react-router-dom";
+import { User, UserContext } from "@/interfaces";
 
-import style from './userCard.module.css';
-
+import style from "./userCard.module.css";
 
 export function UserCard({ user }: { user: User }) {
-    const [, setActiveUserId] = useOutletContext<UserContext>();
-    const navigate = useNavigate();
+	const [, setActiveUserId] = useOutletContext<UserContext>();
+	const navigate = useNavigate();
 
-    const navigateToProfile = () => {
-        setActiveUserId(user.id);
-        navigate(`/profile/${user.id}`);
-    }
+	const navigateToProfile = () => {
+		setActiveUserId(user.id);
+		navigate(`/profile/${user.id}`);
+	};
 
-    return (
-        <div className={style.userCard}>
-            <span><strong>ID</strong>: {user.id}</span>
-            <p className={style.name}><strong>Nom</strong>: {user.userInfos.firstName} {user.userInfos.lastName}</p>
+	return (
+		<div className={style.userCard}>
+			<span>
+				<strong>ID</strong>: {user.id}
+			</span>
 
-            <button className={style.btn} onClick={navigateToProfile}>
-                Voir le profil
-            </button>
-        </div>
-    );
+			<ul className={style.data}>
+				<li>
+					<strong>Nom</strong>: {user.userInfos.firstName} {user.userInfos.lastName}
+				</li>
+				<li>
+					<strong>Age</strong>: {user.userInfos.age}
+				</li>
+			</ul>
+
+			<button className={style.btn} onClick={navigateToProfile}>
+				Voir le profil
+			</button>
+		</div>
+	);
 }
